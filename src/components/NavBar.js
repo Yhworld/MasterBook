35cc27167ba76330a9e02243ea4f3064f6bbd587
Yahya <yahyahussein670@gmail.com>
2022-07-26 15:30:30 +0300

    Add navbar as a child to app.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,13 @@
 import React from 'react'
+//let loadingGif = require("../Night date (1).gif")
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <ul className='nav'>
+        <li>Home</li>
+        <li>Search</li>
+      </ul>
+    </div>
   )
 }
