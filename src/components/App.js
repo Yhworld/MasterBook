d13fb3c9297059ac016a373120cc8e1df6bdbc09
Yahya <yahyahussein670@gmail.com>
2022-07-27 08:25:42 +0300

    imported the header component as a child

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App';
 import { NavBar } from './NavBar';
+import { Header } from './Header';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Header />
     </div>
   )
 }
