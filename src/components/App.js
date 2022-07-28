53d1a391ea31a6685f883fb014df3263e5996e93
Yahya <yahyahussein670@gmail.com>
2022-07-28 17:21:04 +0300

    update the children components by adding a route path

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
+import { Routes, Route } from "react-router-dom";
 import React from 'react';
 import './App';
-import { NavBar } from './NavBar';
-import { Header } from './Header';
+import NavBar from './NavBar'
+import Header from './Header'
+import Search from './Search';
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Header />
-    </div>
+    <>
+    <NavBar />
+    <Routes>
+    <Route exact  path="/" element={ <Header/> } />
+    <Route  exact path="/search" element={ <Search/> } />
+  </Routes>
+  </>
   )
 }
 
 export default App;
+
