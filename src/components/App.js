b08ef6f9b6ded547ff2894b768e632d85a5c8115
Yahya <yahyahussein670@gmail.com>
2022-07-28 21:01:02 +0300

    Fetch data from Api, add functionality to the submit button in the app.js children components

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,38 @@
 import { Routes, Route } from "react-router-dom";
-import React from 'react';
+import React, {useState} from 'react';
 import './App';
 import NavBar from './NavBar'
 import Header from './Header'
 import Search from './Search';
 
 function App() {
+  const [book, setBooks] = useState("")
+  const [result, setResult] = useState([])
+  const [apiKey, setApiKey] = useState("AIzaSyDD9tmSWYx1Kky4x0-REa5sM5KnEL5j6iw")
+
+  function handleChange(event) {
+    const book = event.target.value
+    setBooks(book)
+
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=12")
+    .then(r => r.json())
+    .then((data) => {
+      console.log(data.items);
+      setResult(data.items);
+    })
+  }
   return (
     <>
     <NavBar />
     <Routes>
     <Route exact  path="/" element={ <Header/> } />
-    <Route  exact path="/search" element={ <Search/> } />
+    <Route  exact path="/search" element={ <Search handleChange={handleChange} handleSubmit={handleSubmit} result={result}/> } />
   </Routes>
   </>
   )
 }
 
 export default App;
-
