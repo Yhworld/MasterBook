1626a84d94ff3acca52b4114641d74c9d1fee7cb
Yahya <yahyahussein670@gmail.com>
2022-07-28 16:57:11 +0300

    Update the links by importing the NavLink hook which sets the components path

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,20 @@
 import { GoBook } from "react-icons/go";
+import {NavLink} from "react-router-dom"
 import React from 'react'
 import style from '../App.css'
 //let loadingGif = require("../Night date (1).gif")
 
-export const NavBar = () => {
+ const NavBar = () => {
   return (
     <div style={style} className='navbar'>
       <div style={style} className='logo'>
         <h1>Master<GoBook/></h1>
         </div>
-      <ul style={style} className='nav'>
-        <li>Home</li>
-        <li>Recommended</li>
-      </ul>
-    </div>
-  )
+      <div style={style} className='nav'>
+      <NavLink to={"/"} exact="true">Home</NavLink>
+      <NavLink to={"/search"} exact="true">Search</NavLink>
+      </div>
+      </div>
+       )
 }
+export default NavBar;
