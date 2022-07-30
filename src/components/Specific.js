0122fcddd4776875333e99794a8af173115dd3e5
Yahya <yahyahussein670@gmail.com>
2022-07-30 13:15:58 +0300

    Add the structure and layout of the Specific component

--- src/components/Specific.js
+++ src/components/Specific.js
@@ -1,12 +1,22 @@
-import React from 'react'
-
-const Specific = () => {
-  return (
-    <div>
-        <h2>
-            it worked
-        </h2>
-    </div>
-  )
+import React from 'react';
+const Specific=({show,item,onClose})=>{
+    return(
+        <>
+            <div className="overlay">
+                <div className="overlay-inner">
+                    <button className="close" onClick={onClose}>X</button>
+                    <div className="inner-box">
+                        <div className="info">
+                            <h1>{item.volumeInfo.title}</h1>
+                            <h3>{item.volumeInfo.authors}</h3>
+                            <h4>{item.volumeInfo.publisher}<span>{item.volumeInfo.publishedDate}</span></h4><br/>
+                            <a href={item.volumeInfo.previewLink}><button>More</button></a>
+                        </div>
+                    </div>
+                    <h4 className="description">{item.volumeInfo.description}</h4>
+                </div>
+            </div>
+        </>
+    )
 }
-export default Specific
+export default Specific;
