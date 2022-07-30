180d7d7eb263798aa941d3aa31209d1b5334b92d
Yahya <yahyahussein670@gmail.com>
2022-07-30 13:20:44 +0300

    retrieve the functions previously set on the search page and moved them to an independent component called search results

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Specific from './Specific';
 
-function SearchResult() {
+function SearchResult({result}) {
+    const [show,setShow]=useState(false);
+    const [bookItem,setItem]=useState();
+
+
   return (
-    <div>SearchResult</div>
+    <div className='bookImage'>
+    {result.map(book => (
+      <div className='library'>
+      <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
+      <div>
+        <h2 className='bookText'>{book.volumeInfo.title}</h2>
+        <p>Author : {book.volumeInfo.authors}</p>
+        <p>Ratings : {book.volumeInfo.averageRating} /10 </p>
+        <p>Categories : {book.volumeInfo.categories}</p>
+        <button
+        className="openModalBtn"
+        onClick={()=>{setShow(true);setItem(book)}}>
+        Open
+      </button>
+
+      {show && <Specific show={show} item={bookItem} onClose={()=>setShow(false)} />}
+      </div>
+    </div>
+    ))}
+    </div>
   )
 }
 
