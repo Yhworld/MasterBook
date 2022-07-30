134ee6b02faf5df93335ae4fbc9c804b89782e22
Yahya <yahyahussein670@gmail.com>
2022-07-30 13:47:14 +0300

    move function of search results to its own component and import search result component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BiSearch }  from "react-icons/bi";
-import { Link } from 'react-router-dom';
+import SearchResult from './SearchResult';
 
 const Search = ({handleChange, handleSubmit, result}) => {
 
@@ -13,20 +13,8 @@ const Search = ({handleChange, handleSubmit, result}) => {
       <input onChange={handleChange} type='text' placeholder='Enter Book Title'></input>
       <button className='search' type="submit"><BiSearch /></button>
     </form>
-
-    <div className='bookImage'>
-    {result.map(book => (
-      <div className='library'>
-      <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
-      <div>
-        <h2 className='bookText'>{book.volumeInfo.title}</h2>
-        <p>Author : {book.volumeInfo.authors}</p>
-        <p>Ratings : {book.volumeInfo.averageRating} /10 </p>
-        <p>Categories : {book.volumeInfo.categories}</p>
-        <Link to={`specific/${book.id}`} className='start'>Let's Explore</Link>
-      </div>
-    </div>
-    ))}
+    <div>
+      <SearchResult result={result}/>
     </div>
   </div>
   )
