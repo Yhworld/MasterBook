eabecae1786634f0c86f9c61bf1bcf9a6d1e1d3f
Yahya <yahyahussein670@gmail.com>
2022-07-30 11:53:13 +0300

    import the search result content , and removed the search result content and updated it to search result

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BiSearch }  from "react-icons/bi";
+import { Link } from 'react-router-dom';
 
 const Search = ({handleChange, handleSubmit, result}) => {
 
@@ -16,12 +17,13 @@ const Search = ({handleChange, handleSubmit, result}) => {
     <div className='bookImage'>
     {result.map(book => (
       <div className='library'>
-        
       <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
       <div>
         <h2 className='bookText'>{book.volumeInfo.title}</h2>
         <p>Author : {book.volumeInfo.authors}</p>
         <p>Ratings : {book.volumeInfo.averageRating} /10 </p>
+        <p>Categories : {book.volumeInfo.categories}</p>
+        <Link to={`specific/${book.id}`} className='start'>Let's Explore</Link>
       </div>
     </div>
     ))}
